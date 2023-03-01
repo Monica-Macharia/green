ef5abbc4783cfe75d45673ba10cac1fa1605bbee
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-03-01 12:40:04 +0300

    landing page event listeners

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function hover(){
+//changing the landing page image once the next button is clicked
+let button = document.querySelector("#next")
     
-}
+    button.addEventListener("click", (event)=>{
+        button.style.borderColor="rgb(65, 105, 225)";
+        button.style.borderWidth="medium";
+        let newImage = document.querySelector("#hero")
+        newImage.src= "./images/panel.jpg"
+        document.querySelector("#things").style.display="none";
+    }
+    )
+//changing back the landing page image once the back button is clicked
+let back = document.querySelector("#back")
+    back.addEventListener("click", (event)=>{
+        button.style.borderColor="rgb(65, 105, 225)";
+        button.style.borderWidth="medium";
+        let backImage = document.querySelector("#hero")
+        backImage.src= "./images/mike.jpg"
+        document.querySelector("#things").style.display="inline-block";
+    })
+
+    
